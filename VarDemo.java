b243ba554f58c79cc023a33261338c9a150c21f3
ZYYseven-star <1689859282@qq.com>
2026-05-25 17:17:08 +0800

    day1:hello + 变量练习

---
+++ VarDemo.java
@@ -0,0 +1,13 @@
+public class VarDemo {
+    public static void main(String[] args) {
+        int age = 18;
+        double score = 95.5;
+        boolean isOk = true;
+        char gender = '男';
+
+        System.out.println("年龄：" + age);
+        System.out.println("分数：" + score);
+        System.out.println("是否合格：" + isOk);
+        System.out.println("性别：" + gender);
+    }
+}
